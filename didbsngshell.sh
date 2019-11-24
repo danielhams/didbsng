3a24ed8b18a3037443fcab6007e1aa40dc9a18db
Daniel Hams <daniel.hams@gmail.com>
2019-11-24 08:20:10 +0000

    Test shell - use a temporary dir with the libgcc until gcc is buildable/installable.

--- didbsngshell.sh
+++ didbsngshell.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+
 DIDBSNG_ROOT=/usr/didbsng
 DIDBSNG_BIN=$DIDBSNG_ROOT/bin
 DIDBSNG_LIB=$DIDBSNG_ROOT/lib32
 
-export PATH=$DIDBSNG_BIN:/usr/bin:/bin
-export LD_LIBRARYN32_PATH=$DIDBSNG_LIB:/usr/lib32:/lib32:/usr/lib:/lib
+TMP_STATICGCC_DIR=$DIDBSNG_ROOT/tmplib32
+
+export PATH=$DIDBSNG_BIN:/usr/bin:/bin:/usr/sbin
+export LD_LIBRARYN32_PATH=$DIDBSNG_LIB:$TMP_STATICGCC_DIR:/usr/lib32:/lib32:/usr/lib:/lib
 export PKG_CONFIG_PATH=$DIDBSNG_LIB/pkgconfig
 export PS1='[didbsngshell \u@\h \W]\$ '
 exec bash --norc -i
