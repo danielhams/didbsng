7d28c14470851dd1750feb17227d0100abda48bc
Daniel Hams <daniel.hams@gmail.com>
2019-12-07 01:23:31 +0000

    didbsngshell: add /usr/bsd to the PATH so that builds of tools can pick up the correct 'hostname' binary + values

--- didbsngshell.sh
+++ didbsngshell.sh
@@ -7,7 +7,7 @@ DIDBSNG_LIB=$DIDBSNG_ROOT/lib32
 
 TMP_STATICGCC_DIR=$DIDBSNG_ROOT/tmplib32
 
-export PATH=$DIDBSNG_BIN:/usr/bin:/bin:/usr/sbin
+export PATH=$DIDBSNG_BIN:/usr/bin:/bin:/usr/sbin:/usr/bsd
 export LD_LIBRARYN32_PATH=$DIDBSNG_LIB:$TMP_STATICGCC_DIR:/usr/lib32:/lib32:/usr/lib:/lib
 export PKG_CONFIG_PATH=$DIDBSNG_LIB/pkgconfig
 export PS1='[didbsngshell \u@\h \W]\$ '
