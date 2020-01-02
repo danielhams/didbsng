4bd3f21a4d5b0aea0b480318040c5e73faa08e87
Daniel Hams <daniel.hams@gmail.com>
2020-01-02 03:22:31 +0000

    didbsngshell: include manpath + commented out rpath testing LD_LIBRARYN32_PATH

--- didbsngshell.sh
+++ didbsngshell.sh
@@ -7,8 +7,9 @@ DIDBSNG_LIB=$DIDBSNG_ROOT/lib32
 
 export PATH=$DIDBSNG_BIN:/usr/bin:/bin:/usr/sbin:/usr/bsd
 export LD_LIBRARYN32_PATH=$DIDBSNG_LIB:/usr/lib32:/lib32:/usr/lib:/lib
+#export LD_LIBRARYN32_PATH=/usr/lib32:/lib32:/usr/lib:/lib
 export PKG_CONFIG_PATH=$DIDBSNG_LIB/pkgconfig
-# No man-db yet.
-#export MANPATH=$DIDBSNG_ROOT/man:/usr/share/catman/a_man:/usr/share/catman/g_man:/usr/share/catman/p_man:/usr/share/catman/u_man
+export MANPATH=$DIDBSNG_ROOT/man:/usr/share/catman/a_man:/usr/share/catman/g_man:/usr/share/catman/p_man:/usr/share/catman/u_man
+
 export PS1='[didbsngshell \u@\h \W]\$ '
 exec bash --norc -i
